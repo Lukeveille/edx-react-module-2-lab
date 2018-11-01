073a782c028a97a999f6fa00b5f1b0e9be22c534
Lukeveille <luke@lukeleveille.com>
2018-11-01 17:24:04 -0400

    Sorted out question logic, able to set quiz length

--- application.js
+++ application.js
@@ -5,7 +5,7 @@ function Score(props) {
         <span>Correct: {props.correct}</span>
         <span>Incorrect: {props.incorrect}</span>
       </h2>
-      <h3>{props.currentQuestion} / {props.qCount}</h3>
+      <h3>{props.currentQuestion + 1} / {props.qCount}</h3>
     </div>
   )
 }
@@ -24,7 +24,7 @@ function QuestionDisplay(props) {
   return (
     <div>
       <h1>{props.question}</h1>
-      <GameButtons answers={props.answers} handleClick={props.handleClick} shuffle={props.shuffle}/>
+      <GameButtons correctCount={props.correctCount} answers={props.answers} handleClick={props.handleClick} shuffle={props.shuffle}/>
       <Score correct={props.correct} incorrect={props.incorrect} qCount={props.qCount} currentQuestion={props.currentQuestion}/>
     </div>
   )
@@ -32,35 +32,61 @@ function QuestionDisplay(props) {
 // First answer is the correct one (answers are shuffled before they print)
 function QuestionData() {
   return [
-    {question: 'what is e?',
+    {question: 'what is a?',
     answers: ['a', 'b', 'c', 'd']},
-    {question: 'what is i?',
+    {question: 'what is e?',
     answers: ['e', 'f', 'g', 'h']},
-    {question: 'what is m?',
+    {question: 'what is i?',
     answers: ['i', 'j', 'k', 'l']},
-    {question: 'what is q?',
+    {question: 'what is m?',
     answers: ['m', 'n', 'o', 'p']},
-    {question: 'what is u?',
+    {question: 'what is q?',
     answers: ['q', 'r', 's', 't']},
-    {question: 'what is y?',
+    {question: 'what is u?',
     answers: ['u', 'v', 'w', 'x']},
-    {question: 'what is 3?',
+    {question: 'what is y?',
     answers: ['y', 'z', '1', '2']},
+    {question: 'what is 3?',
+    answers: ['3', '4', '5', '6']},
+    {question: 'what is 7?',
+    answers: ['7', '8', '9', '10']},
+    {question: 'what is 11?',
+    answers: ['11', '12', '13', '14']},
+    {question: 'what is 15?',
+    answers: ['15', '16', '17', '18']},
+    {question: 'what is 19?',
+    answers: ['19', '20', '21', '22']},
   ]
+}
+function EndGame() {
+  return {
+    question: 'End of Quiz',
+    answers: ['Try Again']
+  }
 }
 
 class TriviaApp extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {
-      questions: this.shuffledData(),
+    this.state = this.initialize();
+    this.handleClick = this.handleClick.bind(this);
+  }
+  
+  // Number of questions to be randomly selected from the dataset
+  quizLength() {
+    return 10;
+  }
+
+  initialize() {
+    return {
+      questions: this.shuffledData(this.quizLength()),
+      qCount: this.quizLength(),
       correct: 0,
       incorrect: 0,
-      qCount: QuestionData().length,
-      currentQuestion: 1
+      currentQuestion: 0,
+      game: true
     }
-    this.handleClick = this.handleClick.bind(this);
   }
 
   shuffle(a) {
@@ -73,33 +99,41 @@ class TriviaApp extends React.Component {
     }
     return a;
   }
-  shuffledData() {
-    var questions = QuestionData();
-    // questions.map(question => {
-    //   question.answers = this.shuffle(question.answers)
-    // });
+  shuffledData(n) {
+    let data = QuestionData();
+    let questions = data.slice(0, n);
     return this.shuffle(questions);
   }
 
   handleClick(answer) {
     this.setState((prevState) => { 
-      // if (prevState.currentQuestion < prevState.qCount) {
       var output = {};
-      if (answer == prevState.questions[prevState.currentQuestion-1].answers[0]) {
-        output.correct = prevState.correct + 1
+      if (prevState.game === true) {
+        if (answer === prevState.questions[prevState.currentQuestion].answers[0]) {
+          output.correct = prevState.correct + 1;
+        } else {
+          output.incorrect = prevState.incorrect + 1;
+        }
+
+        if (prevState.currentQuestion < prevState.qCount-1) {
+          output.currentQuestion = prevState.currentQuestion + 1;
+        } else {
+          output.game = false;
+        }
       } else {
-        output.incorrect = prevState.correct + 1
+        output = this.initialize();
       }
-      console.log(answer)
-      if (prevState.currentQuestion < prevState.qCount) {
-        console.log(prevState.questions[prevState.currentQuestion-1].answers[0])
-        output.currentQuestion = prevState.currentQuestion + 1
-      }
-      return output
+      return output;
     });
   }
+
   render() {
-    return <QuestionDisplay shuffle={this.shuffle} handleClick={this.handleClick} question={this.state.questions[this.state.currentQuestion-1].question} answers={this.state.questions[this.state.currentQuestion-1].answers} correct={this.state.correct} incorrect={this.state.incorrect} qCount={this.state.qCount} currentQuestion={this.state.currentQuestion}/>
+    if (this.state.game) {
+      var questionAnswer = this.state.questions[this.state.currentQuestion];
+    } else {
+      var questionAnswer = EndGame();
+    }
+    return <QuestionDisplay correctCount={this.state.correct + this.state.incorrect} game={this.state.game} shuffle={this.shuffle} handleClick={this.handleClick} question={questionAnswer.question} answers={questionAnswer.answers} correct={this.state.correct} incorrect={this.state.incorrect} qCount={this.state.qCount} currentQuestion={this.state.currentQuestion}/>
   }
 }
 
